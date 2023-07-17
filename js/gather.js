2f4249fbd3a82eac90aa1a2cfdab13ef906d2f21
minipuding <sijie.x@foxmail.com>
2023-07-17 22:47:34 +0800

    update

--- js/gather.js
+++ js/gather.js
@@ -2,7 +2,7 @@ function submitForm() {
     var name = $('#name').val();
     var email = $('#email').val();
     var institution = $('#institution').val();
-    var token = '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';
+    var token = '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';
     $.ajax({
       url: 'https://api.github.com/repos/fastmrt/fastmrt.github.io/dispatches',
       type: 'POST',
